2c5b25375ef4253e2b710027538e99158c9f3686
Roberto Manzo <45563029+RobManzo@users.noreply.github.com>
2021-05-04 16:07:12 +0200

    None

--- controllers/cameriere/cameriere.py
+++ controllers/cameriere/cameriere.py
@@ -13,16 +13,3 @@ psNames = [
     'ps0', 'ps1', 'ps2', 'ps3',
     'ps4', 'ps5', 'ps6', 'ps7'
 ]
-
-for i in range(8):
-    ps.append(robot.getDevice(psNames[i]))
-    ps[i].enable(32)
-
-while robot.step(32) != -1:
-    left_motor.setVelocity(5.0)
-    right_motor.setVelocity(5.0)
-    
-    psValues = []
-    for i in range(8):
-        psValues.append(ps[i].getValue())
-        print(psValues[i])
