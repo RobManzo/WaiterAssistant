ceff00e541abb207767d167c92c76e467b2f503f
Roberto Manzo <45563029+RobManzo@users.noreply.github.com>
2021-07-04 19:06:31 +0200

    Position (continue)

--- controllers/cameriere/Positioning.py
+++ controllers/cameriere/Positioning.py
@@ -23,54 +23,54 @@ class Positioning:
     def getPosition(self):
         return self.position
     
-    def setPosition(self, position):
-        x = self.position.getX()
-        y = self.position.getY()
-        if x > 0 and x < Map.HEIGHT - 1:
-            self.position.setX(position.x)
-        if y > 0 and y < Map.WIDTH - 1:
-            self.position.setY(position.y)
+    #def setPosition(self, position):
+    #    x = self.position.getX()
+    #    y = self.position.getY()
+    #    if x > 0 and x < Map.HEIGHT - 1:
+    #        self.position.setX(position.x)
+    #    if y > 0 and y < Map.WIDTH - 1:
+    #        self.position.setY(position.y)
     
     def update(self):
         self.updateOrientation()
-        self.updatePosition()
+        #self.updatePosition()
     
-    def updateWheelTraveledDistance(self):
-        # get radiants from wheel
-        radFLW = self.positionsensor.getLeftDistance()
-        radFRW = self.positionsensor.getRightDistance()
-
-        # compute distance traveled
-        self.leftWheelDistance = radFLW * WHEEL_RADIUS
-        self.rightWheelDistance = radFRW * WHEEL_RADIUS
-
-    def updatePosition(self):
-        
-        speed = self.actuators.getSpeed()
-
-        if speed != 0:
-            # get actual float map position
-            x = self.position.x
-            y = self.position.y
-
-            # 72 = 0.50 m/s * speed/MAX_SPEED [1.8] / 40 step/s / 0.5 m
-            # linearMove = ((0.50 * (speed/MAX_SPEED)) / 40) * 2
-            linearMove = speed/72
-
-            # compass decimal digits
-            precision = 2
-
-            turnCoeficent = 1
-            # if turning you need to do less meters in order to change position in the map
-            steeringAngle = self.actuators.getAngle()
-            if abs(steeringAngle) == 0.57:
-                turnCoeficent = 1.2
-
-            # update position    
-            newX = x - round(self.compass.getXComponent(), precision) * linearMove * turnCoeficent
-            newY = y + round(self.compass.getYComponent(), precision) * linearMove * turnCoeficent
-        
-            self.setPosition(Position(newX,newY))
+    #def updateWheelTraveledDistance(self):
+    #    # get radiants from wheel
+    #    radFLW = self.positionsensor.getLeftDistance()
+    #    radFRW = self.positionsensor.getRightDistance()
+#
+    #    # compute distance traveled
+    #    self.leftWheelDistance = radFLW * WHEEL_RADIUS
+    #    self.rightWheelDistance = radFRW * WHEEL_RADIUS
+#
+    #def updatePosition(self):
+    #    
+    #    speed = self.actuators.getSpeed()
+#
+    #    if speed != 0:
+    #        # get actual float map position
+    #        x = self.position.x
+    #        y = self.position.y
+#
+    #        # 72 = 0.50 m/s * speed/MAX_SPEED [1.8] / 40 step/s / 0.5 m
+    #        # linearMove = ((0.50 * (speed/MAX_SPEED)) / 40) * 2
+    #        linearMove = speed/72
+#
+    #        # compass decimal digits
+    #        precision = 2
+#
+    #        turnCoeficent = 1
+    #        # if turning you need to do less meters in order to change position in the map
+    #        steeringAngle = self.actuators.getAngle()
+    #        if abs(steeringAngle) == 0.57:
+    #            turnCoeficent = 1.2
+#
+    #        # update position    
+    #        newX = x - round(self.compass.getXComponent(), precision) * linearMove * turnCoeficent
+    #        newY = y + round(self.compass.getYComponent(), precision) * linearMove * turnCoeficent
+    #    
+    #        self.setPosition(Position(newX,newY))
 
     
     
